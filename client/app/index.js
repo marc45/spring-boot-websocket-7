555cd3ab8354151486b163f42691444e22704bbd
Renjith G R <grenjithr@gmail.com>
2016-03-26 22:46:03 +0530

    Adding react router and simple routing

--- client/app/index.js
+++ client/app/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './containers/App';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
 const store = createStore(rootReducer);
 
 render (
-    <Provider store={store}>
-        <App />
+    <Provider store = { store }>
+        <Router history = { browserHistory } routes = { routes }/>
     </Provider>,
     document.getElementById('example')
 );
